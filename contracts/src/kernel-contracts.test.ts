04d1df215e7d2d6bb6d30fb245c34d3c0f788a2b
OpenThrottle Executor <executor@openthrottle.local>
2000-01-01 00:00:00 +0000

    OpenThrottle integrated checkpoint

--- contracts/src/kernel-contracts.test.ts
+++ contracts/src/kernel-contracts.test.ts
@@ -29,6 +29,7 @@ import {
   type ExecutionPlanContractV2,
   type ReviewFindingV1,
 } from "./index.js";
+import { assertProviderSchemaCompatibility } from "./test-support/provider-schema.js";
 
 const sha = (character: string): string => character.repeat(64);
 const subject = (character: string): string => character.repeat(40);
@@ -150,49 +151,6 @@ function naturalExecutionPlan(): ExecutionPlanContractV2 {
     ],
     commands: [{ name: "test", unit: "implementation" }],
   };
-}
-
-function assertProviderSchemaCompatibility(value: unknown, path = "$"): void {
-  expect(value, `${path} must be a schema object`).toBeTypeOf("object");
-  expect(value, `${path} must be a schema object`).not.toBeNull();
-  expect(Array.isArray(value), `${path} must be a schema object`).toBe(false);
-  const schema = value as Record<string, unknown>;
-  expect(
-    typeof schema.type === "string" || Array.isArray(schema.anyOf) || typeof schema.$ref === "string",
-    `${path} must declare type, anyOf, or $ref`,
-  ).toBe(true);
-  expect(schema, `${path} uses unsupported uniqueItems`).not.toHaveProperty("uniqueItems");
-  if (Object.hasOwn(schema, "const")) {
-    const expectedType = schema.const === null
-      ? "null"
-      : Array.isArray(schema.const)
-        ? "array"
-        : typeof schema.const;
-    expect(schema.type, `${path}.type for const`).toBe(expectedType);
-  }
-  if (schema.type === "object") {
-    expect(schema.additionalProperties, `${path}.additionalProperties`).toBe(false);
-    expect(schema.properties, `${path}.properties`).toBeTypeOf("object");
-    expect(schema.required, `${path}.required`).toEqual(
-      Object.keys(schema.properties as Record<string, unknown>).sort(),
-    );
-  }
-  if (Array.isArray(schema.anyOf)) {
-    schema.anyOf.forEach((entry, index) => assertProviderSchemaCompatibility(entry, `${path}.anyOf[${index}]`));
-  }
-  if (schema.items !== undefined) {
-    assertProviderSchemaCompatibility(schema.items, `${path}.items`);
-  }
-  if (schema.properties && typeof schema.properties === "object" && !Array.isArray(schema.properties)) {
-    for (const [key, entry] of Object.entries(schema.properties)) {
-      assertProviderSchemaCompatibility(entry, `${path}.properties.${key}`);
-    }
-  }
-  if (schema.$defs && typeof schema.$defs === "object" && !Array.isArray(schema.$defs)) {
-    for (const [key, entry] of Object.entries(schema.$defs)) {
-      assertProviderSchemaCompatibility(entry, `${path}.$defs.${key}`);
-    }
-  }
 }
 
 describe("semantic result candidates", () => {
--- contracts/src/runtime-artifacts.test.ts
+++ contracts/src/runtime-artifacts.test.ts
@@ -14,6 +14,7 @@ import {
   validateEvalDefinition,
   validateSemanticResultSchema,
 } from "./index.js";
+import { assertProviderSchemaCompatibility } from "./test-support/provider-schema.js";
 
 const contractsRoot = new URL("..", import.meta.url).pathname;
 const repositoryRoot = join(contractsRoot, "..");
@@ -37,49 +38,6 @@ function authoredSemanticSchemas() {
       return validateEvalDefinition(document.toJS({ maxAliasCount: 0 }), { source: path }).value.result;
     })
     .sort((left, right) => left.id < right.id ? -1 : left.id > right.id ? 1 : 0);
-}
-
-function assertProviderSchemaCompatibility(value: unknown, path = "$"): void {
-  expect(value, `${path} must be a schema object`).toBeTypeOf("object");
-  expect(value, `${path} must be a schema object`).not.toBeNull();
-  expect(Array.isArray(value), `${path} must be a schema object`).toBe(false);
-  const schema = value as Record<string, unknown>;
-  expect(
-    typeof schema.type === "string" || Array.isArray(schema.anyOf) || typeof schema.$ref === "string",
-    `${path} must declare type, anyOf, or $ref`,
-  ).toBe(true);
-  expect(schema, `${path} uses unsupported uniqueItems`).not.toHaveProperty("uniqueItems");
-  if (Object.hasOwn(schema, "const")) {
-    const expectedType = schema.const === null
-      ? "null"
-      : Array.isArray(schema.const)
-        ? "array"
-        : typeof schema.const;
-    expect(schema.type, `${path}.type for const`).toBe(expectedType);
-  }
-  if (schema.type === "object") {
-    expect(schema.additionalProperties, `${path}.additionalProperties`).toBe(false);
-    expect(schema.properties, `${path}.properties`).toBeTypeOf("object");
-    expect(schema.required, `${path}.required`).toEqual(
-      Object.keys(schema.properties as Record<string, unknown>).sort(),
-    );
-  }
-  if (Array.isArray(schema.anyOf)) {
-    schema.anyOf.forEach((entry, index) => assertProviderSchemaCompatibility(entry, `${path}.anyOf[${index}]`));
-  }
-  if (schema.items !== undefined) {
-    assertProviderSchemaCompatibility(schema.items, `${path}.items`);
-  }
-  if (schema.properties && typeof schema.properties === "object" && !Array.isArray(schema.properties)) {
-    for (const [key, entry] of Object.entries(schema.properties)) {
-      assertProviderSchemaCompatibility(entry, `${path}.properties.${key}`);
-    }
-  }
-  if (schema.$defs && typeof schema.$defs === "object" && !Array.isArray(schema.$defs)) {
-    for (const [key, entry] of Object.entries(schema.$defs)) {
-      assertProviderSchemaCompatibility(entry, `${path}.$defs.${key}`);
-    }
-  }
 }
 
 describe("generated runtime artifacts", () => {
